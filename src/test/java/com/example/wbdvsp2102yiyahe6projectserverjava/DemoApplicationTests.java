04ef2a6b60ceb0fefd43bb7a2b75a4cd3a1f566c
yiyahe6project <50472854+yiyahe6project@users.noreply.github.com>
2021-02-02 03:13:09 +0800

    inital push

---
+++ src/test/java/com/example/wbdvsp2102yiyahe6projectserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2102yiyahe6projectserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
